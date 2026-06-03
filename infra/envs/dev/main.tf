468f3af378d17cb5dd549572221638975c5a46e5
Luka Bakhtadze <lbakhtadze@bog.ge>
2026-06-03 17:28:48 +0400

    fix incorrect attribute value for second route table id

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -33,7 +33,7 @@ resource "aws_route_table" "second_rt" {
 
 resource "aws_route_table_association" "public_subnet_asso" {
     subnet_id = aws_subnet.public_subnet.id
-    route_table_id = aws_route_table.second_rt
+    route_table_id = aws_route_table.second_rt.id
 }
 
 # Create subnets
