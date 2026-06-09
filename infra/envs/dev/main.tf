e902e21c588ef1e655060476f07d2b66eae337bd
Luka Bakhtadze <lbakhtadze@bog.ge>
2026-06-09 13:54:18 +0400

    add asw_security_group and attach to EC2

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -62,8 +62,49 @@ resource "aws_instance" "jupiter" {
   ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
   instance_type = "t3.micro"
   subnet_id = aws_subnet.public_subnet.id
-
+  vpc_security_group_ids = [aws_security_group.jupiter_sg.id]
+  metadata_options {
+    http_tokens = "required"
+  }
   tags = {
     Name = "jupiter"
   }
+}
+
+# SG
+resource "aws_security_group" "jupiter_sg" {
+    vpc_id = aws_vpc.main.id
+    name   = "jupiter-sg" 
+    ingress {
+        description = "HTTPS from anywhere"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "HTTP from anywhere"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "SSH from my IP only"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks  = ["91.209.131.22/32"]
+    }
+    egress {
+        description = "All outbound"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "jupiter-sg"
+    }
 }
